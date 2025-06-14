e8e24eb383561c4aca9e81c5f19d96168b1a6568
svaddi <sukumarvaddi@gmail.com>
2025-06-14 09:21:20 -0500

    my work

---
+++ 3_section/2_mastering_print/mastering_print.py
@@ -0,0 +1,8 @@
+print("String in double quotes")
+print('String in single quotes')
+print(1+2)
+print(7*6)
+print()
+
+# Variable number of arguments to the print function
+print("The End", "or is it?", "Keep watching to learn more about python", 3)
---
+++ 3_section/3_strings/1_strings.py
@@ -0,0 +1,10 @@
+# String concatneation
+print("Hello" + " " + "World")
+
+greeting = "Hello"
+
+name = input("What is your name? ")
+rollNo = input("What is your roll number? ")
+rollNoInInteger = int(rollNo)
+
+print(greeting + " " + name + " with a roll number " + str(rollNoInInteger))
