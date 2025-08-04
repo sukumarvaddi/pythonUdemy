408d0dabdc5a0d9e8febac75958219ec74029ee8
Sukumar Vaddi <svaddi@findhelp.com>
2025-08-04 17:04:56 -0500

    sort and sorted

---
+++ 5_Lists_Tuples/12_sorted.py
@@ -0,0 +1,12 @@
+pangram = "The quick brown fox jumps over the lazy dog"
+
+letters = sorted(pangram.casefold())
+print(letters)
+
+numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
+
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+
+# sort method is mutating and sorted function is not.
