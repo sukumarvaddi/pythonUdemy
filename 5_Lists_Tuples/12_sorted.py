f0835eb6176f49c7bd743704ac2fdb52022e557a
Sukumar Vaddi <svaddi@findhelp.com>
2025-08-06 19:11:04 -0500

    Lists and Tuples updates

--- 5_Lists_Tuples/12_sorted.py
+++ 5_Lists_Tuples/12_sorted.py
@@ -1,6 +1,8 @@
+# sort method is mutating and sorted function is not.
+
 pangram = "The quick brown fox jumps over the lazy dog"
 
-letters = sorted(pangram.casefold())
+letters = sorted(pangram, key=str.casefold)
 print(letters)
 
 numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
@@ -8,5 +10,8 @@ numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 
+names = ["Alan", "Adam", "Wes", "will", "Albert",
+         "Steven", "Sarah", "Zoe", "Susan", "Becky"]
 
-# sort method is mutating and sorted function is not.
+names.sort(key=str.casefold)
+print(names)
